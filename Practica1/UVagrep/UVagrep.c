7a31204b4c7320eab200631479d96d7b117453a8
aceituna <aceituna@maquinon>
2025-02-18 22:57:53 +0100

    fix stdin error

--- Practica1/UVagrep/UVagrep.c
+++ Practica1/UVagrep/UVagrep.c
@@ -1,13 +1,67 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+// i'll implement in the future my own strstr() function
+bool iswordfound(char *line, char *word) {
+
+  if (strlen(line) < strlen(word))
+    return false;
+
+  return strstr(line, word);
+}
+
+void processfile(FILE *file, char *word) {
+
+  size_t n = 0; 
+  size_t bytesread;
+  char *line = NULL;
+
+  while ((bytesread = getline(&line, &n, file)) != -1) {
+    if (iswordfound(line, word))
+      printf("%s", line);
+  }
+
+  free(line);
+  
+  return;
+}
+
+FILE *openfile(char *filename) {
+
+  FILE *file;
+  
+  if (!(file = fopen(filename, "r"))) {
+    printf("UVagrep: cannot open file\n");
+    exit(1);
+  }
+
+  return file;
+}
 
 int main (int argc, char *argv[]) {
 
+  FILE *file;
+  char *word;
+  
   if (argc == 1) {
     printf("UVagrep: searchterm [file ...]\n");
     return(1);
   }
 
-  printf("%s", argv[1]);
+  word = argv[1];
 
+  if (argc == 2) {
+    processfile(stdin, word);
+    return(0);
+  }
+  
+  for (int i = 2; i < argc; i++) {
+    file = openfile(argv[i]);
+    processfile(file, word);
+  }
+  
   return(0);
 }
